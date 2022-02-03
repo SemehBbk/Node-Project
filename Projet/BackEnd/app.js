45b9e240e4965b8c6df5e445607a0c8aeaf52c39
semeh boubaker <semehboubaker0@gmail.com>
2022-02-03 01:21:47 +0100

    commit

--- Projet/BackEnd/app.js
+++ Projet/BackEnd/app.js
@@ -1,3 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use('/api/stuff', (req, res, next) => {
   const stuff = [
     {
@@ -19,3 +30,4 @@ app.use('/api/stuff', (req, res, next) => {
   ];
   res.status(200).json(stuff);
 });
+module.exports = app;
