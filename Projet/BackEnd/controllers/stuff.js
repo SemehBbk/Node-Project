f17f402cf22ea1b5df6b542e050b7f6828c81baf
semeh <semehboubaker@gmail.com>
2022-02-08 17:56:29 +0100

    commit

--- Projet/BackEnd/controllers/stuff.js
+++ Projet/BackEnd/controllers/stuff.js
@@ -9,34 +9,15 @@ exports.createThing = (req, res, next) => {
     price: req.body.price,
     userId: req.body.userId
   });
-  thing.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-};
+  thing.save()
+    .then(() => {res.status(201).json({message: 'Post saved successfully!' });})
+    .catch((error) => {res.status(400).json({error: error});}
+  );};
 
 exports.getOneThing = (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id
-  }).then(
-    (thing) => {
-      res.status(200).json(thing);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
+  Thing.findOne({_id: req.params.id})
+  .then((thing) => {res.status(200).json(thing);})
+  .catch((error) => {res.status(404).json({error: error});}
   );
 };
 
@@ -49,47 +30,44 @@ exports.modifyThing = (req, res, next) => {
     price: req.body.price,
     userId: req.body.userId
   });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  Thing.updateOne({_id: req.params.id}, thing)
+    .then(() => {res.status(201).json({message: 'Thing updated successfully!'});})
+    .catch((error) => {res.status(400).json({error: error});});
 };
 
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
+  Thing.findOne({ _id: req.params.id }).then(
+    (thing) => {
+      if (!thing) {
+        res.status(404).json({
+          error: new Error('No such Thing!')
+        });
+      }
+      if (thing.userId !== req.auth.userId) {
+        res.status(400).json({
+          error: new Error('Unauthorized request!')
+        });
+      }
+      Thing.deleteOne({ _id: req.params.id }).then(
+        () => {
+          res.status(200).json({
+            message: 'Deleted!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
     }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  )
 };
 
 exports.getAllStuff = (req, res, next) => {
-  Thing.find().then(
-    (things) => {
-      res.status(200).json(things);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  Thing.find()
+  .then((things) => { res.status(200).json(things); })
+  .catch((error) => {res.status(400).json({error: error });});
 };
+
--- Projet/BackEnd/controllers/user.js
+++ Projet/BackEnd/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
-const user = require('../models/user')
+const User = require('../models/User')
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
@@ -16,22 +17,26 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        }
-        bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
-            if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
-            }
-            res.status(200).json({
-              userId: user._id,
-              token: 'TOKEN'
-            });
-          })
-          .catch(error => res.status(500).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' }
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
